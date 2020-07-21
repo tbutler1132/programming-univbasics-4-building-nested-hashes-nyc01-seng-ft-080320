a3e7a8498c2638048ce1fa225ad518ab6dde7da2
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-21 14:57:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,8 @@ end
 def monopoly_with_second_tier
   first_tier = {
     :railroads => {
-      :pieces => 4
-      :rent_in_dollars => {}
+      :pieces => 4,
+      :rent_in_dollars => {},
       :names => {}
     }
   }
